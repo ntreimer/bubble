4e6be335018437018aa149c93207059bbbd1564f
Nate Treimer <natetreimer@gmail.com>
2021-04-26 14:08:47 -0500

    activity on load

--- src/components/ActivityPage/ActivityPage.jsx
+++ src/components/ActivityPage/ActivityPage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import {React, useState, useEffect} from "react";
-import Button from '@material-ui/core/Button';
+import { React, useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 // This is one of our simplest components
@@ -9,37 +9,51 @@ import Typography from "@material-ui/core/Typography";
 // or even care what the redux state is'
 
 function ActivityPage() {
-  const [activity, setActivity] = useState('');
+  const [activity, setActivity] = useState("");
   const [object, setObject] = useState({});
 
+  useEffect(() => {
+    getActivity();
+  }, []);
+
   const getActivity = () => {
-    axios.get('/api/bored').then((res) => {
-      console.log(res.data);
-      setActivity(res.data.activity);
-      setObject(res.data);
-    }).catch((err) => {
-      console.log(err);
-    })
-  }
+    axios
+      .get("/api/bored")
+      .then((res) => {
+        console.log(res.data);
+        setActivity(res.data.activity);
+        setObject(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const saveActivity = () => {
-    axios.post('/api/activity', object).then((res) => {
-      console.log(res);
-      alert('POSTed activity');
-    }).catch((err) => {
-      console.log(err);
-    })
-  }
+    axios
+      .post("/api/activity", object)
+      .then((res) => {
+        console.log(res);
+        alert("POSTed activity");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="container">
       <div>
         <Typography variant="h4">Activity here:</Typography>
-        <br/>
+        <br />
         <Typography variant="h5">{activity}</Typography>
-        <br/>
-        <Button color="primary" variant="contained" onClick={getActivity}>Get Activity</Button>
-        <Button color="primary" variant="contained" onClick={saveActivity}>Save Activity</Button>
+        <br />
+        <Button color="primary" variant="contained" onClick={getActivity}>
+          Get Activity
+        </Button>
+        <Button color="primary" variant="contained" onClick={saveActivity}>
+          Save Activity
+        </Button>
       </div>
     </div>
   );

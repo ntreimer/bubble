ca93d637b2037f9d507ad56a0fc1a4b96359dd10
Nate Treimer <natetreimer@gmail.com>
2021-04-28 14:37:56 -0500

    cleaned up app routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -42,31 +42,21 @@ function App() {
 
           {/* Visiting localhost:3000/home will show the home page. */}
           <ProtectedRoute
-            // shows AboutPage at all times (logged in or not)
             exact
             path="/add-activity"
           >
             <AddActivityPage />
           </ProtectedRoute>
           <ProtectedRoute
-            // shows AboutPage at all times (logged in or not)
             exact
             path="/home"
           >
             <ActivityPage />
           </ProtectedRoute>
-
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
 
           <ProtectedRoute
 
@@ -93,16 +83,6 @@ function App() {
           {/* When a value is supplied for the authRedirect prop the user will
             be redirected to the path supplied when logged in, otherwise they will
             be taken to the component and path supplied. */}
-          <ProtectedRoute
-            // with authRedirect:
-            // - if logged in, redirects to "/user"
-            // - else shows LoginPage at /login
-            exact
-            path="/login"
-            authRedirect="/user"
-          >
-            <LoginPage />
-          </ProtectedRoute>
 
           <ProtectedRoute
             // with authRedirect:
@@ -110,7 +90,7 @@ function App() {
             // - else shows RegisterPage at "/registration"
             exact
             path="/registration"
-            authRedirect="/user"
+            authRedirect="/home"
           >
             <RegisterPage />
           </ProtectedRoute>
@@ -120,8 +100,8 @@ function App() {
             // - if logged in, redirects to "/user"
             // - else shows LandingPage at "/home"
             exact
-            path="/home"
-            authRedirect="/user"
+            path="/login"
+            authRedirect="/home"
           >
             <LandingPage />
           </ProtectedRoute>
